d19218d29eabc583d2adc102be1447101fc4e598
DenyZhirkov <denyzhirkov@yandex.ru>
2015-06-10 12:05:30 +0300

    f1

--- mmodal.js
+++ mmodal.js
@@ -4,11 +4,11 @@
 ;var Mmodal = function(p){
 
 	var _cfg = {};
-	var _hover = document.getElementById('mmodal');
+	var _hover = document.getElementById('Mmodal');
 	if(_hover==undefined){
 		_hover = document.createElement('div');
-		_hover.setAttribute('style','position:fixed;top:0;left:0;width:100%;height:100%;background:#222;z-index:10000;opacity:0.75;');
-		_hover.setAttribute('id','mmodal');
+		_hover.setAttribute('style','position:fixed;top:0;left:0;width:100%;height:100%;background:#222;z-index:10000;opacity:0.75;display:none;');
+		_hover.setAttribute('id','Mmodal');
 		document.body.appendChild(_hover);
 	}
 	var _modal = document.createElement('div');
@@ -28,38 +28,38 @@
 	_cfg.title = (p!=undefined)? p.title || 'Mmodal!' : 'Mmodal!' ;
 	_cfg.body = (p!=undefined)? p.body || false : false;
 	_cfg.buttons = (p!=undefined)? p.buttons || false : false;
-	_cfg.class = (p!=undefined)? p.class || 'mmodal' : 'mmodal';
+	_cfg.class = 'Mmodal '+((p!=undefined)? p.class || '' : '');
 
 	_title.innerHTML = _cfg.title;
-	_title.setAttribute('class','mmodal-title');
+	_title.setAttribute('class','Mmodal-title');
 	_modal.appendChild(_title);
 
 	if(_cfg.body){
 		_body.innerHTML = _cfg.body;
-		_body.setAttribute('class','mmodal-body');
+		_body.setAttribute('class','Mmodal-body');
 		_modal.appendChild(_body);
 	}
 
-	_buttons.setAttribute('class','mmodal-buttons');
+	_buttons.setAttribute('class','Mmodal-buttons');
 	if(!_cfg.buttons){
 		var _btn = document.createElement('div');
 		_btn.innerHTML = 'Да';
-		_btn.setAttribute('class','mmodal-button');
+		_btn.setAttribute('class','Mmodal-button');
 		_btn.onclick = function(){_modal.hide()};
 		_buttons.appendChild(_btn);
 	}else{
 		_cfg.buttons.reverse();
 		_modal.buttons = [];
 		for(var i = _cfg.buttons.length - 1; i >= 0; i--) {
-			var a = document.createElement('div');
-			a.setAttribute('class','mmodal-button');
-			a.handler = _cfg.buttons[i].handler;
-			a.onclick = function(){
+			var __a = document.createElement('div');
+			__a.setAttribute('class','Mmodal-button '+(_cfg.buttons[i].class||''));
+			__a.handler = _cfg.buttons[i].handler;
+			__a.onclick = function(){
 				this.handler(_modal);
 			};
-			a.style.width = 100/_cfg.buttons.length+'%';
-			a.innerHTML = _cfg.buttons[i].value;
-			_modal.buttons.unshift(a);
+			__a.style.width = 100/_cfg.buttons.length+'%';
+			__a.innerHTML = _cfg.buttons[i].value;
+			_modal.buttons.unshift(__a);
 			_buttons.appendChild(_modal.buttons[0]);
 		};
 	}
